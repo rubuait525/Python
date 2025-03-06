1ea8c1acbd7c0f9b1c347bcee0a5f9299c6b6344
Rubuait Rahman <rubuait@Rubuaits-iMac.local>
2025-03-06 11:13:46 -0600

    HW05

---
+++ HW/hw05.py
@@ -0,0 +1,16 @@
+#HW_Python_User_Input_In_Defined_Function:
+
+x = input("Python User Input: ")
+base = int(input("base: "))
+height = int(input("height: "))
+
+def areaOfParallelogram ():
+    area = (base*height)
+    print(f"The Area of Parallelogram is: {area}")
+    
+areaOfParallelogram()
+
+
+
+
+     
